4a5fe94d291f1285042e336d2f037f740ee384a8
avsxntos <thur.vitalino@gmail.com>
2024-06-24 19:46:03 -0300

    criação do arquivo banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        /* Criação de Scanner para fazer a criação do armazenamento de dados inseridos pelo usuário*/ 
+        Scanner sc = new Scanner(System.in);
+
+         /* Criação de Varíaveis */
+        int numero;
+        String texto;
+        String nomeCliente;
+        float saldo;
+
+
+    }
+}
